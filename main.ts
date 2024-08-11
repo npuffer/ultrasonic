d9da5c2982d70634050613db768afe0318628dec
npuffer <puffernc@gmail.com>
2024-08-11 10:22:44 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,6 @@ let u = 0
 basic.forever(function () {
     u = maqueen.Ultrasonic(PingUnit.Centimeters)
     basic.showNumber(u)
+    basic.pause(1000)
+    basic.showIcon(IconNames.LeftTriangle)
 })
